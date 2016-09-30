c73cbb94bdbf2dce6b4eb8f396e454b1f0f91de4
郑波 <zhengbo@pv25.com>
2016-09-30 17:40:02 +0800

    x

--- server.go
+++ server.go
@@ -9,6 +9,8 @@ import (
 	"runtime"
 	"flag"
 	"fmt"
+	"time"
+	"github.com/qgweb/glib/convert"
 )
 
 var (
@@ -36,7 +38,7 @@ func main() {
 		func(conn redcon.Conn, cmd redcon.Command) {
 			switch strings.ToLower(string(cmd.Args[0])) {
 			default:
-				conn.WriteError("ERR unknown command '" + string(cmd.Args[0]) + "'")
+				CmdHelp(conn, db, cmd)
 			case "ping":
 				conn.WriteString("PONG")
 			case "quit":
@@ -50,8 +52,18 @@ func main() {
 				CmdDel(conn, db, cmd)
 			case "keys":
 				CmdKeys(conn, db, cmd)
+			case "scan":
+				CmdScan(conn, db, cmd)
+			case "scanrange":
+				CmdScanRange(conn, db, cmd)
 			case "info":
 				CmdInfo(conn, db, cmd)
+			case "index":
+				CmdCreateIndex(conn, db, cmd)
+			case "indexjson":
+				CmdCreateIndexJson(conn, db, cmd)
+			case "delindex":
+				CmdDeleteIndex(conn, db, cmd)
 			}
 		},
 		func(conn redcon.Conn) bool {
@@ -68,7 +80,34 @@ func main() {
 		log.Fatal(err)
 	}
 }
-
+func CmdHelp(conn redcon.Conn, db *buntdb.DB, cmd redcon.Command) {
+	var helpstr = `
+	set key value [ttl(s)]
+		eg: set name 11 10
+		eg: set name 11
+	get key
+		eg: get name
+	del key
+		eg: del name
+	keys key
+		eg: keys *
+		eg: keys n*
+	index indexkey pattern
+		eg: index name *
+		eg: index name n*
+	indexjson indexkey  pattern path
+		eg: indexjson name * name
+	scan indexkey
+		eg: scan name
+	scanrange indexkey greaterOrEqual lessThan
+		eg: scan name '{name:10}' '{name:20}'
+	delindex indexkey
+		eg: delindex name
+	info
+		eg: info
+	`
+	conn.WriteString(helpstr)
+}
 func CmdGet(conn redcon.Conn, db *buntdb.DB, cmd redcon.Command) {
 	if len(cmd.Args) != 2 {
 		conn.WriteError("ERR wrong number of arguments for '" + string(cmd.Args[0]) + "' command")
@@ -107,12 +146,19 @@ func CmdDel(conn redcon.Conn, db *buntdb.DB, cmd redcon.Command) {
 }
 
 func CmdSet(conn redcon.Conn, db *buntdb.DB, cmd redcon.Command) {
-	if len(cmd.Args) != 3 {
+	if len(cmd.Args) < 3 {
 		conn.WriteError("ERR wrong number of arguments for '" + string(cmd.Args[0]) + "' command")
 		return
 	}
 	db.Update(func(tx *buntdb.Tx) error {
-		_, _, err := tx.Set(string(cmd.Args[1]), string(cmd.Args[2]), nil)
+		option := &buntdb.SetOptions{}
+		if len(cmd.Args) > 3 {
+			option.Expires = true
+			option.TTL = time.Second * time.Duration(convert.ToInt64(string(cmd.Args[3])))
+		} else {
+			option = nil
+		}
+		_, _, err := tx.Set(string(cmd.Args[1]), string(cmd.Args[2]), option)
 		if err != nil {
 			conn.WriteString("NO")
 			return nil
@@ -144,6 +190,88 @@ func CmdKeys(conn redcon.Conn, db *buntdb.DB, cmd redcon.Command) {
 		}
 		return nil
 	})
+}
+
+func CmdCreateIndex(conn redcon.Conn, db *buntdb.DB, cmd redcon.Command) {
+	if len(cmd.Args) != 3 {
+		conn.WriteError("ERR wrong number of arguments for '" + string(cmd.Args[0]) + "' command")
+		return
+	}
+	if err := db.CreateIndex(string(cmd.Args[1]), string(cmd.Args[2]), buntdb.IndexString); err != nil {
+		conn.WriteError("ERR " + err.Error())
+		return
+	}
+	conn.WriteString("OK")
+}
+
+func CmdCreateIndexJson(conn redcon.Conn, db *buntdb.DB, cmd redcon.Command) {
+	if len(cmd.Args) != 4 {
+		conn.WriteError("ERR wrong number of arguments for '" + string(cmd.Args[0]) + "' command")
+		return
+	}
+	if err := db.CreateIndex(string(cmd.Args[1]), string(cmd.Args[2]), buntdb.IndexJSON(string(cmd.Args[3]))); err != nil {
+		conn.WriteError("ERR " + err.Error())
+		return
+	}
+	conn.WriteString("OK")
+}
+
+func CmdScan(conn redcon.Conn, db *buntdb.DB, cmd redcon.Command) {
+	if len(cmd.Args) != 2 {
+		conn.WriteError("ERR wrong number of arguments for '" + string(cmd.Args[0]) + "' command")
+		return
+	}
+	db.View(func(tx *buntdb.Tx) error {
+		var vals = make([]string, 0, 100)
+		tx.Ascend(string(cmd.Args[1]), func(key, value string) bool {
+			vals = append(vals, key + "\t" + value)
+			return true
+		})
+		if len(vals) > 0 {
+			conn.WriteArray(len(vals))
+			for _, v := range vals {
+				conn.WriteBulkString(v)
+			}
+		} else {
+			conn.WriteNull()
+		}
+		return nil
+	})
+}
+
+func CmdScanRange(conn redcon.Conn, db *buntdb.DB, cmd redcon.Command) {
+	if len(cmd.Args) != 4 {
+		conn.WriteError("ERR wrong number of arguments for '" + string(cmd.Args[0]) + "' command")
+		return
+	}
+	db.View(func(tx *buntdb.Tx) error {
+		var vals = make([]string, 0, 100)
+		tx.AscendRange(string(cmd.Args[1]), string(cmd.Args[2]), string(cmd.Args[3]), func(key, value string) bool {
+			vals = append(vals, key + "\t" + value)
+			return true
+		})
+		if len(vals) > 0 {
+			conn.WriteArray(len(vals))
+			for _, v := range vals {
+				conn.WriteBulkString(v)
+			}
+		} else {
+			conn.WriteNull()
+		}
+		return nil
+	})
+}
+
+func CmdDeleteIndex(conn redcon.Conn, db *buntdb.DB, cmd redcon.Command) {
+	if len(cmd.Args) != 2 {
+		conn.WriteError("ERR wrong number of arguments for '" + string(cmd.Args[0]) + "' command")
+		return
+	}
+	if err := db.DropIndex(string(cmd.Args[1])); err != nil {
+		conn.WriteError("ERR " + err.Error())
+		return
+	}
+	conn.WriteString("OK")
 }
 
 func CmdInfo(conn redcon.Conn, db *buntdb.DB, cmd redcon.Command) {
